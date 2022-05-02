ee422cc0acfc28b77a9bf1ef1437dae219321fb3
Remtim <remtim@protonmail.com>
2022-05-02 10:54:00 +0200

    A few additional changes to form, nothing special

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
@@ -54,7 +55,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 75);
             this.button1.TabIndex = 1;
-            this.button1.Text = "1";
+            this.button1.Text = "2";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -63,7 +64,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 75);
             this.button3.TabIndex = 2;
-            this.button3.Text = "1";
+            this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -72,7 +73,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 75);
             this.button4.TabIndex = 3;
-            this.button4.Text = "1";
+            this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -81,7 +82,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 75);
             this.button5.TabIndex = 4;
-            this.button5.Text = "1";
+            this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -90,7 +91,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 75);
             this.button6.TabIndex = 5;
-            this.button6.Text = "1";
+            this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
@@ -99,7 +100,7 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 75);
             this.button7.TabIndex = 6;
-            this.button7.Text = "1";
+            this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
@@ -108,9 +109,8 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(75, 75);
             this.button8.TabIndex = 7;
-            this.button8.Text = "1";
+            this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -120,13 +120,23 @@
             this.button9.TabIndex = 8;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 53);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "CalculationResult";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -139,6 +149,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -153,5 +164,6 @@
         private Button button7;
         private Button button8;
         private Button button9;
+        private TextBox textBox1;
     }
 }
